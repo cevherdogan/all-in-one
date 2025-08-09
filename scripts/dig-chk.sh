8eef74ff9e0eef31f31ba90cc0158f5273f1a46a
Cevher Dogan <cevherd@gmail.com>
2025-08-08 23:01:29 -0400

    Added .github/workflows/deploy-pages.yml and dig script to check DNS and basic networking

---
+++ scripts/dig-chk.sh
@@ -0,0 +1,10 @@
+dig +short all-in-one.foundral.tech CNAME
+dig +short cevherdogan.github.io
+curl -I https://cevherdogan.github.io/all-in-one/
+
+
+curl -I https://all-in-one.foundral.tech/
+curl -I https://all-in-one.foundral.tech/site/index.html
+
+
+
